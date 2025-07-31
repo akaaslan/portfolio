858df4bfac7ded55a99e38e7e5428cc38e3a7106
akaaslan <akaan7368@gmail.com>
2025-07-31 15:05:42 +0300

    Vercel deployment fix.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // GitHub repository adınızla değiştirin
+  base: '/', // Vercel için root path
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
